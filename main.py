b095fa712cb82a8803d0b7a01e0bde38cf4288e7
Amir Sharapov <amir@amirfounder.com>
2022-01-29 21:35:31 -0600

    frontend connects to backend. pagination still required

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from src.app import run_app
+from src.api.app import app
+from src.data.seed import seed_data
+from src.data.setup import setup_schema
+
+
+def main():
+  setup_schema()
+  seed_data()
+  app.run(port=8085, debug=True, use_reloader=False)
+
 
 if __name__ == '__main__':
-  run_app()
+  main()
